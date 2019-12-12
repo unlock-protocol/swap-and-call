44c89412fe56b282a14293f71a9685fafcf269e8
NickCuso <hardlydiff@gmail.com>
2019-12-11 16:10:26 -0800

    Initial implementation

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  networks: {
+    development: {
+      gas: 6721974,
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.14",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 2000000
+        }
+      }
+    }
+  },
+  plugins: ["solidity-coverage"],
+  mocha: {
+    reporter: "eth-gas-reporter",
+    useColors: true,
+    reporterOptions: {
+      currency: "USD",
+      excludeContracts: ["Migrations"],
+      gasPrice: 5
+    }
+  }
+};
