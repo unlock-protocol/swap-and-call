cfa395f24ec4b54f08d1cae792cdf5df1a805aae
NickCuso <hardlydiff@gmail.com>
2019-12-14 13:18:38 -0800

    +tokenToToken

--- test/uniswapAndCall.js
+++ test/uniswapAndCall.js
@@ -3,52 +3,67 @@ const { protocols, tokens } = require("hardlydifficult-ethereum-contracts");
 const UniswapAndCall = artifacts.require("UniswapAndCall.sol");
 const truffleAssert = require("truffle-assertions");
 
+async function createExchange(uniswap, token, tokenOwner) {
+  let tx = await uniswap.createExchange(token.address, {
+    from: tokenOwner
+  });
+  const exchange = await protocols.uniswap.getExchange(
+    web3,
+    tx.logs[0].args.exchange
+  );
+  await token.mint(tokenOwner, "1000000000000000000000000", {
+    from: tokenOwner
+  });
+  await token.approve(exchange.address, -1, { from: tokenOwner });
+  await exchange.addLiquidity(
+    "1",
+    "1000000000000000000000000",
+    Math.round(Date.now() / 1000) + 60,
+    {
+      from: tokenOwner,
+      value: web3.utils.toWei("1", "ether")
+    }
+  );
+
+  return exchange;
+}
+
 contract("uniswapAndCall", accounts => {
   const owner = accounts[0];
+  const testAccount = accounts[2];
   const keyPrice = web3.utils.toWei("0.00042", "ether");
-  let token;
-  let exchange;
+  let targetToken;
+  let sourceToken;
+  let targetExchange;
+  let sourceExchange;
   let lock;
   let uniswapAndCall;
+  let callData;
 
   beforeEach(async () => {
     // Token
-    token = await tokens.sai.deploy(web3, owner);
+    targetToken = await tokens.sai.deploy(web3, owner);
+    sourceToken = await tokens.sai.deploy(web3, owner);
 
     // Uniswap exchange with liquidity for testing
     const uniswap = await protocols.uniswap.deploy(web3, owner);
-    let tx = await uniswap.createExchange(token.address, {
-      from: owner
-    });
-    exchange = await protocols.uniswap.getExchange(
-      web3,
-      tx.logs[0].args.exchange
-    );
-    await token.mint(owner, "1000000000000000000000000", { from: owner });
-    await token.approve(exchange.address, -1, { from: owner });
-    await exchange.addLiquidity(
-      "1",
-      "1000000000000000000000000",
-      Math.round(Date.now() / 1000) + 60,
-      {
-        from: owner,
-        value: web3.utils.toWei("1", "ether")
-      }
-    );
+    targetExchange = await createExchange(uniswap, targetToken, owner);
+    sourceExchange = await createExchange(uniswap, sourceToken, owner);
 
     // Lock priced in ERC-20 tokens
-    const unlockProtocol = await protocols.unlock.deploy(web3, owner);
-    tx = await unlockProtocol.createLock(
-      60 * 60 * 24, // expirationDuration (in seconds) of 1 day
-      token.address,
-      keyPrice,
-      100, // maxNumberOfKeys
-      "Test Lock", // lockName
+    lock = await protocols.unlock.createTestLock(
+      web3,
+      accounts[9], // Unlock Protocol owner
+      accounts[1], // Lock owner
       {
-        from: owner
+        tokenAddress: targetToken.address,
+        keyPrice
       }
     );
-    lock = await protocols.unlock.getLock(web3, tx.logs[1].args.newLockAddress);
+    callData = web3.eth.abi.encodeFunctionCall(
+      lock.abi.find(e => e.name === "purchaseFor"),
+      [testAccount]
+    );
 
     // UniswapAndCall
     uniswapAndCall = await UniswapAndCall.new(uniswap.address);
@@ -56,47 +71,81 @@ contract("uniswapAndCall", accounts => {
 
   it("Sanity check: Can't purchase keys with ether", async () => {
     await truffleAssert.fails(
-      lock.purchaseFor(accounts[2], {
-        from: accounts[2],
-        value: await exchange.getEthToTokenOutputPrice(keyPrice)
+      lock.purchaseFor(testAccount, {
+        from: testAccount,
+        value: await targetExchange.getEthToTokenOutputPrice(keyPrice)
       })
     );
   });
 
   describe("Purchase with tokens", () => {
     beforeEach(async () => {
-      await token.mint(accounts[2], "1000000000000000000000000", {
+      await targetToken.mint(testAccount, "1000000000000000000000000", {
         from: owner
       });
-      await token.approve(lock.address, -1, { from: accounts[2] });
+      await targetToken.approve(lock.address, -1, { from: testAccount });
     });
 
     it("Sanity check: Can purchase keys with tokens", async () => {
-      await lock.purchaseFor(accounts[2], {
-        from: accounts[2]
+      await lock.purchaseFor(testAccount, {
+        from: testAccount
       });
     });
   });
 
   it("Can purchase keys with ether via UniswapAndCall", async () => {
-    const callData = web3.eth.abi.encodeFunctionCall(
-      lock.abi.find(e => e.name === "purchaseFor"),
-      [accounts[2]]
-    );
     await uniswapAndCall.uniswapEthAndCall(
-      token.address,
+      targetToken.address,
       keyPrice,
       lock.address,
       callData,
       {
-        from: accounts[2],
-        value: new BigNumber(await exchange.getEthToTokenOutputPrice(keyPrice))
+        from: testAccount,
+        value: new BigNumber(
+          await targetExchange.getEthToTokenOutputPrice(keyPrice)
+        )
           .times(1.1)
           .dp(0, BigNumber.ROUND_UP)
       }
     );
 
-    const hasKey = await lock.getHasValidKey(accounts[2]);
+    const hasKey = await lock.getHasValidKey(testAccount);
     assert.equal(hasKey, true);
+  });
+
+  describe("started with sourceTokens", () => {
+    beforeEach(async () => {
+      await sourceToken.mint(testAccount, "1000000000000000000000000", {
+        from: owner
+      });
+      await sourceToken.approve(uniswapAndCall.address, -1, {
+        from: testAccount
+      });
+    });
+
+    it.only("Can purchase keys with sourceTokens via UniswapAndCall", async () => {
+      const ethBought = await targetExchange.getEthToTokenOutputPrice(keyPrice);
+      console.log(ethBought.toString()); // 4212637916
+      const tokenReserve = await sourceToken.balanceOf(sourceExchange.address);
+      console.log(tokenReserve.toString());
+      const inputPrice = await sourceExchange.getTokenToEthOutputPrice(
+        ethBought
+      );
+      console.log(inputPrice.toString());
+      await uniswapAndCall.uniswapTokenAndCall(
+        sourceToken.address,
+        await sourceToken.balanceOf(testAccount),
+        targetToken.address,
+        keyPrice,
+        lock.address,
+        callData,
+        {
+          from: testAccount
+        }
+      );
+
+      const hasKey = await lock.getHasValidKey(testAccount);
+      assert.equal(hasKey, true);
+    });
   });
 });
