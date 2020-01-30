524e1635a5ce42448cd7693ef996e2e433b29cd5
Hardly Difficult <hardlydiff@gmail.com>
2020-01-30 00:40:16 -0800

    Refactor implementation & add tests (#84)
    * Swap And Call
    
    * Lint
    
    * Add pausable

--- test/uniswapAndCall.js
+++ test/uniswapAndCall.js
@@ -1,9 +1,10 @@
 const BigNumber = require("bignumber.js");
 const { protocols, tokens } = require("hardlydifficult-ethereum-contracts");
-const UniswapAndCall = artifacts.require("UniswapAndCall.sol");
-const truffleAssert = require("truffle-assertions");
+const SwapAndCall = artifacts.require("SwapAndCall.sol");
+const { reverts, fails } = require("truffle-assertions");
+const makeCalls = require("./helpers/makeCalls");
 
-async function createExchange(uniswap, token, tokenOwner) {
+async function createAndFundExchange(uniswap, token, tokenOwner) {
   let tx = await uniswap.createExchange(token.address, {
     from: tokenOwner
   });
@@ -28,7 +29,7 @@ async function createExchange(uniswap, token, tokenOwner) {
   return exchange;
 }
 
-contract("uniswapAndCall", accounts => {
+contract("swapAndCall", accounts => {
   const owner = accounts[0];
   const testAccount = accounts[2];
   const keyPrice = web3.utils.toWei("0.00042", "ether");
@@ -37,8 +38,7 @@ contract("uniswapAndCall", accounts => {
   let targetExchange;
   let sourceExchange;
   let tokenLock, ethLock;
-  let uniswapAndCall;
-  let callData;
+  let swapAndCall;
 
   beforeEach(async () => {
     // Token
@@ -47,8 +47,8 @@ contract("uniswapAndCall", accounts => {
 
     // Uniswap exchange with liquidity for testing
     const uniswap = await protocols.uniswap.deploy(web3, owner);
-    targetExchange = await createExchange(uniswap, targetToken, owner);
-    sourceExchange = await createExchange(uniswap, sourceToken, owner);
+    targetExchange = await createAndFundExchange(uniswap, targetToken, owner);
+    sourceExchange = await createAndFundExchange(uniswap, sourceToken, owner);
 
     ethLock = await protocols.unlock.createTestLock(
       web3,
@@ -68,17 +68,12 @@ contract("uniswapAndCall", accounts => {
         keyPrice
       }
     );
-    callData = web3.eth.abi.encodeFunctionCall(
-      tokenLock.abi.find(e => e.name === "purchaseFor"),
-      [testAccount]
-    );
 
-    // UniswapAndCall
-    uniswapAndCall = await UniswapAndCall.new(uniswap.address);
+    swapAndCall = await SwapAndCall.new();
   });
 
   it("Sanity check: Can't purchase keys with ether", async () => {
-    await truffleAssert.fails(
+    await fails(
       tokenLock.purchaseFor(testAccount, {
         from: testAccount,
         value: await targetExchange.getEthToTokenOutputPrice(keyPrice)
@@ -86,39 +81,68 @@ contract("uniswapAndCall", accounts => {
     );
   });
 
-  describe("Purchase with tokens", () => {
-    beforeEach(async () => {
-      await targetToken.mint(testAccount, "1000000000000000000000000", {
-        from: owner
-      });
-      await targetToken.approve(tokenLock.address, -1, { from: testAccount });
+  it("Sanity check: Can purchase keys with tokens", async () => {
+    await targetToken.mint(testAccount, "1000000000000000000000000", {
+      from: owner
     });
-
-    it("Sanity check: Can purchase keys with tokens", async () => {
-      await tokenLock.purchaseFor(testAccount, {
-        from: testAccount
-      });
+    await targetToken.approve(tokenLock.address, -1, { from: testAccount });
+    await tokenLock.purchaseFor(testAccount, {
+      from: testAccount
     });
   });
 
-  it("Can purchase keys with ether via UniswapAndCall", async () => {
-    await uniswapAndCall.uniswapEthAndCall(
-      targetToken.address,
-      keyPrice,
-      tokenLock.address,
-      callData,
-      {
+  describe("Purchase key priced in tokens with ether", () => {
+    beforeEach(async () => {
+      const ethValue = new BigNumber(
+        await targetExchange.getEthToTokenOutputPrice(keyPrice)
+      )
+        .times(1.1)
+        .dp(0, BigNumber.ROUND_UP);
+      const calls = [];
+      // Swap ETH provided into the target token
+      calls.push({
+        contract: targetExchange.address,
+        callData: targetExchange.contract.methods
+          .ethToTokenSwapOutput(keyPrice, -1)
+          .encodeABI(),
+        value: ethValue
+      });
+      // Approve the Lock to spend funds help by the swapAndCall contract
+      calls.push({
+        contract: targetToken.address,
+        callData: targetToken.contract.methods
+          .approve(tokenLock.address, keyPrice) // using the exact amount when possible saves gas
+          .encodeABI()
+      });
+      // Call purchase on the Lock
+      calls.push({
+        contract: tokenLock.address,
+        callData: tokenLock.contract.methods
+          .purchaseFor(testAccount)
+          .encodeABI()
+      });
+      // Tokens to refund in case keyPrice went down
+      // (n/a with v5 but may be again with v6)
+      await makeCalls(swapAndCall, undefined, 0, calls, targetToken.address, {
         from: testAccount,
-        value: new BigNumber(
-          await targetExchange.getEthToTokenOutputPrice(keyPrice)
-        )
-          .times(1.1)
-          .dp(0, BigNumber.ROUND_UP)
-      }
-    );
+        value: ethValue.toFixed()
+      });
+    });
 
-    const hasKey = await tokenLock.getHasValidKey(testAccount);
-    assert.equal(hasKey, true);
+    it("Has a valid key", async () => {
+      const hasKey = await tokenLock.getHasValidKey(testAccount);
+      assert.equal(hasKey, true);
+    });
+
+    it("Has no ether left behind", async () => {
+      const balance = await web3.eth.getBalance(swapAndCall.address);
+      assert.equal(balance, 0);
+    });
+
+    it("Has no tokens left behind", async () => {
+      const balance = await targetToken.balanceOf(swapAndCall.address);
+      assert.equal(balance, 0);
+    });
   });
 
   describe("started with sourceTokens", () => {
@@ -126,42 +150,192 @@ contract("uniswapAndCall", accounts => {
       await sourceToken.mint(testAccount, "1000000000000000000000000", {
         from: owner
       });
-      await sourceToken.approve(uniswapAndCall.address, -1, {
+      // Infinite approval for the tokenSpender allows us to call this once for many swapAndCalls
+      await sourceToken.approve(await swapAndCall.tokenSpender(), -1, {
         from: testAccount
       });
     });
 
-    it("Can purchase token keys with sourceTokens via UniswapAndCall", async () => {
-      await uniswapAndCall.uniswapTokenAndCall(
-        sourceToken.address,
-        await sourceToken.balanceOf(testAccount),
-        targetToken.address,
-        keyPrice,
-        tokenLock.address,
-        callData,
-        {
-          from: testAccount
-        }
-      );
+    describe("Purchase key priced in tokens with a different token", () => {
+      beforeEach(async () => {
+        const calls = [];
+        const sourceValue = new BigNumber(
+          await sourceExchange.getTokenToEthOutputPrice(
+            await targetExchange.getEthToTokenOutputPrice(keyPrice)
+          )
+        )
+          .times(1.1)
+          .dp(0, BigNumber.ROUND_UP);
 
-      const hasKey = await tokenLock.getHasValidKey(testAccount);
-      assert.equal(hasKey, true);
+        // Approve the exchange to take source tokens from the swapAndCall contract
+        calls.push({
+          contract: sourceToken.address,
+          callData: sourceToken.contract.methods
+            .approve(sourceExchange.address, sourceValue.toFixed()) // Exact approval saves gas
+            .encodeABI()
+        });
+        // Swap tokens provided into the target token
+        calls.push({
+          contract: sourceExchange.address,
+          callData: sourceExchange.contract.methods
+            .tokenToTokenSwapOutput(keyPrice, -1, -1, -1, targetToken.address)
+            .encodeABI()
+        });
+        // Approve the Lock to spend funds help by the swapAndCall contract
+        calls.push({
+          contract: targetToken.address,
+          callData: targetToken.contract.methods
+            .approve(tokenLock.address, keyPrice) // Exact approval saves gas
+            .encodeABI()
+        });
+        // Call purchase on the Lock
+        calls.push({
+          contract: tokenLock.address,
+          callData: tokenLock.contract.methods
+            .purchaseFor(testAccount)
+            .encodeABI()
+        });
+        // Tokens to refund in case keyPrice went down
+        // (n/a with v5 but may be again with v6)
+        await makeCalls(
+          swapAndCall,
+          sourceToken.address,
+          sourceValue,
+          calls,
+          targetToken.address,
+          {
+            from: testAccount
+          }
+        );
+      });
+
+      it("Has a valid key", async () => {
+        const hasKey = await tokenLock.getHasValidKey(testAccount);
+        assert.equal(hasKey, true);
+      });
+
+      it("Has no ether left behind", async () => {
+        const balance = await web3.eth.getBalance(swapAndCall.address);
+        assert.equal(balance, 0);
+      });
+
+      it("Has no source tokens left behind", async () => {
+        const balance = await sourceToken.balanceOf(swapAndCall.address);
+        assert.equal(balance, 0);
+      });
+
+      it("Has no target tokens left behind", async () => {
+        const balance = await targetToken.balanceOf(swapAndCall.address);
+        assert.equal(balance, 0);
+      });
     });
 
-    it("Can purchase eth keys with sourceTokens via UniswapAndCall", async () => {
-      await uniswapAndCall.uniswapTokenToEthAndCall(
-        sourceToken.address,
-        await sourceToken.balanceOf(testAccount),
-        keyPrice,
-        ethLock.address,
-        callData,
-        {
-          from: testAccount
-        }
-      );
+    describe("Purchase key priced in eth with a token", () => {
+      beforeEach(async () => {
+        const calls = [];
+        const sourceValue = new BigNumber(
+          await sourceExchange.getTokenToEthOutputPrice(keyPrice)
+        )
+          .times(1.1)
+          .dp(0, BigNumber.ROUND_UP);
+        // Approve the exchange to take source tokens from the swapAndCall contract
+        calls.push({
+          contract: sourceToken.address,
+          callData: sourceToken.contract.methods
+            .approve(sourceExchange.address, sourceValue.toFixed()) // Exact approval saves gas
+            .encodeABI()
+        });
+        // Swap tokens provided into the target token
+        calls.push({
+          contract: sourceExchange.address,
+          callData: sourceExchange.contract.methods
+            .tokenToEthSwapOutput(keyPrice, -1, -1)
+            .encodeABI()
+        });
+        // Call purchase on the Lock
+        calls.push({
+          contract: ethLock.address,
+          callData: ethLock.contract.methods
+            .purchaseFor(testAccount)
+            .encodeABI(),
+          value: keyPrice
+        });
+        await makeCalls(
+          swapAndCall,
+          sourceToken.address,
+          sourceValue,
+          calls,
+          undefined,
+          {
+            from: testAccount
+          }
+        );
+      });
 
-      const hasKey = await ethLock.getHasValidKey(testAccount);
-      assert.equal(hasKey, true);
+      it("Has a valid key", async () => {
+        const hasKey = await ethLock.getHasValidKey(testAccount);
+        assert.equal(hasKey, true);
+      });
+
+      it("Has no ether left behind", async () => {
+        const balance = await web3.eth.getBalance(swapAndCall.address);
+        assert.equal(balance, 0);
+      });
+
+      it("Has no source tokens left behind", async () => {
+        const balance = await sourceToken.balanceOf(swapAndCall.address);
+        assert.equal(balance, 0);
+      });
+    });
+
+    describe("Purchase fails when paused", () => {
+      beforeEach(async () => {
+        await swapAndCall.pause();
+      });
+
+      it("should fail", async () => {
+        const calls = [];
+        const sourceValue = new BigNumber(
+          await sourceExchange.getTokenToEthOutputPrice(keyPrice)
+        )
+          .times(1.1)
+          .dp(0, BigNumber.ROUND_UP);
+        // Approve the exchange to take source tokens from the swapAndCall contract
+        calls.push({
+          contract: sourceToken.address,
+          callData: sourceToken.contract.methods
+            .approve(sourceExchange.address, sourceValue.toFixed())
+            .encodeABI()
+        });
+        // Swap tokens provided into the target token
+        calls.push({
+          contract: sourceExchange.address,
+          callData: sourceExchange.contract.methods
+            .tokenToEthSwapOutput(keyPrice, -1, -1)
+            .encodeABI()
+        });
+        // Call purchase on the Lock
+        calls.push({
+          contract: ethLock.address,
+          callData: ethLock.contract.methods
+            .purchaseFor(testAccount)
+            .encodeABI(),
+          value: keyPrice
+        });
+        await reverts(
+          makeCalls(
+            swapAndCall,
+            sourceToken.address,
+            sourceValue,
+            calls,
+            undefined,
+            {
+              from: testAccount
+            }
+          ),
+          "Pausable: paused"
+        );
+      });
     });
   });
 });
